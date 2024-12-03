a164b1b23888e7a88dee6991c245f017d2ca8149
petr-frycak-upol <petr.frycak@upol.cz>
2024-12-03 16:36:48 +0100

    second commit

---
+++ tcc.py
@@ -0,0 +1,26 @@
+from scipy.optimize import fsolve
+import math
+
+c0_HCl = 0.1
+V0_HCl = 0.1
+c_NaOH = 0.1
+Kw = 1e-14
+
+def equations(vars, precalculated_args):
+    H, OH = vars
+    Na, Cl = precalculated_args
+    eq1 = Kw - H * OH
+    eq2 = Na + H - Cl - OH
+    return [eq1, eq2]
+
+def precalculate(c0_HCl: float, V0_HCl: float, c_NaOH: float, V_NaOH: float) -> (float, float):
+    Cl = c0_HCl * V0_HCl / (V0_HCl + V_NaOH)
+    Na = c_NaOH * V_NaOH / (V0_HCl + V_NaOH)
+    return (Cl, Na)
+
+def calculate_pH(V_NaOH: float) -> float:
+    initial_H_OH = [1e-7, 1e-7]
+    solution = fsolve(equations, initial_H_OH, args=precalculate(c0_HCl, V0_HCl, c_NaOH, V_NaOH))
+    H, OH = solution
+
+    return -math.log10(H)
