f2dff8b25333c2aa4f79d240954887730eec36ba
Jakub-Koza <kuba.koza.5352@seznam.cz>
2024-12-10 15:33:43 +0100

    Tuples Volume and [H]

--- tcc_top_level.py
+++ tcc_top_level.py
@@ -1,3 +1,18 @@
 from tcc import calculate_pH
 
-print(calculate_pH(V_NaOH=0))
+def titration_curve(v_start: float, v_end: float, step: float) -> list:
+    result = []
+    V_NaOH_values = [v_start + i * step for i in range(int((v_end - v_start) / step) + 1)]
+    for V_NaOH in V_NaOH_values:
+        pH = calculate_pH(V_NaOH)
+        result.append((V_NaOH, pH))
+    return result
+
+v_start = 0.00
+v_end = 0.2
+step = 0.001
+
+titration_data = titration_curve(v_start, v_end, step)
+
+for volume, pH in titration_data:
+    print(f"Volume NaOH: {volume:.3} l pH: {pH:.2}")
