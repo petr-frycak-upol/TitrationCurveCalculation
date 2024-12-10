cb182392a6ec3700aee5a5a5d9849ea3e75de37e
Jakub-Koza <kuba.koza.5352@seznam.cz>
2024-12-10 16:21:29 +0100

    Final

--- tcc_top_level.py
+++ tcc_top_level.py
@@ -3,8 +3,16 @@ from tcc import calculate_pH
 def titration_curve(v_start: float, v_end: float, step: float) -> list:
     result = []
     V_NaOH_values = [v_start + i * step for i in range(int((v_end - v_start) / step) + 1)]
+    initial_H_OH = [1E-1, 1E-13]
+    after_eq_point = False
     for V_NaOH in V_NaOH_values:
-        pH = calculate_pH(V_NaOH)
+        pH = calculate_pH(V_NaOH, initial_H_OH)
+        if V_NaOH<0.1:
+            initial_H_OH = [pH, 1E-14 / pH]
+        else:
+            if not after_eq_point: initial_H_OH = [1E-8, 1E-6]
+            else: initial_H_OH = [pH, 1E-14 / pH]
+            after_eq_point = True
         result.append((V_NaOH, pH))
     return result
 
