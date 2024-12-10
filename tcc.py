4e97d07636b97742b95f6eb7510a3bd122c8e90c
petr-frycak-upol <petr.frycak@upol.cz>
2024-12-10 15:34:33 +0100

    první verze 10.12.2024

--- tcc.py
+++ tcc.py
@@ -6,8 +6,8 @@ V0_HCl = 0.1
 c_NaOH = 0.1
 Kw = 1e-14
 
-def equations(vars, precalculated_args):
-    H, OH = vars
+def equations(unknowns, *precalculated_args):
+    H, OH = unknowns
     Cl, Na = precalculated_args
     eq1 = Kw - H * OH
     eq2 = Na + H - Cl - OH
@@ -16,11 +16,12 @@ def equations(vars, precalculated_args):
 def precalculate(c0_HCl: float, V0_HCl: float, c_NaOH: float, V_NaOH: float) -> (float, float):
     Cl = c0_HCl * V0_HCl / (V0_HCl + V_NaOH)
     Na = c_NaOH * V_NaOH / (V0_HCl + V_NaOH)
-    return [Cl, Na]
+    return Cl, Na
 
 def calculate_pH(V_NaOH: float) -> float:
-    initial_H_OH = [1E-4, 1E-10]
+    initial_H_OH = [1e-7, 1e-7]
     solution = fsolve(equations, initial_H_OH, args=precalculate(c0_HCl, V0_HCl, c_NaOH, V_NaOH))
     H, OH = solution
 
+    return H
     return -math.log10(H)
--- tcc_top_level.py
+++ tcc_top_level.py
@@ -1,3 +1,12 @@
 from tcc import calculate_pH
+import pandas as pd
 
-print(calculate_pH(V_NaOH=0))
+#A = [(0, 1.0), (0.05, 1.1), (0.10, 1.2)]
+#df = pd.DataFrame(A)
+#df.to_clipboard(header=False, index = False)
+
+print(calculate_pH(V_NaOH=0.05))
+
+
+
+
